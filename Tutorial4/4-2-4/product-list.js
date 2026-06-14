1ed4a6c7cc826b59b36457cf87052a567b6c0942
ai2525desu <umi08062525@gmail.com>
2026-06-14 20:54:44 +0900

    課題＋実務風練習問題の回答終了

--- Tutorial4/4-2-4/product-list.js
+++ Tutorial4/4-2-4/product-list.js
@@ -83,3 +83,116 @@ function soldOut(productList, productId) {
 
 const updatedProducts = soldOut(products, 2);
 console.log(updatedProducts);
+
+// 練習問題２
+const users = [
+    {
+        id: 1,
+        name: "田中",
+        active: true,
+    },
+    {
+        id: 2,
+        name: "佐藤",
+        active: false,
+    },
+    {
+        id: 3,
+        name: "鈴木",
+        active: true,
+    },
+];
+
+// アクティブなユーザーだけ取得する関数
+function getActiveUser(userList) {
+    return userList.filter((user) => user.active === true);
+}
+console.log(getActiveUser(users));
+
+// 指定したIDのユーザーをactive:falseに変更する関数
+function deactivateUser(userList, userId) {
+    return userList.map((user) => {
+        if (user.id === userId) {
+            return {
+                ...user,
+                active: false,
+            };
+        }
+        return user;
+    });
+}
+
+console.log(deactivateUser(users, 1));
+
+// 実務より練習問題；企業サイトのお問い合わせ管理画面
+// 要望：①未対応のお問い合わせだけを確認したい②対応済みに変更したい
+// 下記はデータ
+const inquiries = [
+    {
+        id: 1,
+        name: "田中",
+        subject: "料金について",
+        handled: false,
+    },
+    {
+        id: 2,
+        name: "佐藤",
+        subject: "サービス内容について",
+        handled: true,
+    },
+    {
+        id: 3,
+        name: "鈴木",
+        subject: "見積もり依頼",
+        handled: false,
+    },
+];
+
+// 未対応の問合せを取得する関数
+function getUnansweredInquiries(inquiriesList) {
+    return inquiriesList.filter((inquiry) => inquiry.handled === false);
+}
+console.log(getUnansweredInquiries(inquiries));
+// お問い合わせ内容に対応した場合にupdateして問合せ済みに変更する関数
+function completeInquiry(inquiriesList, inquiryId) {
+    return inquiriesList.map((inquiry) => {
+        if (inquiry.id === inquiryId) {
+            return {
+                ...inquiry,
+                handled: true,
+            };
+        }
+        return inquiry;
+    });
+}
+console.log(completeInquiry(inquiries, 3));
+
+// 実務風練習問題②：お知らせ管理画面
+// 公開中のお知らせだけを取得し、一覧画面にタイトルだけ表示したい
+// 期待結果：[ "夏季休業のお知らせ","新サービス開始" ]
+const notices = [
+    {
+        id: 1,
+        title: "夏季休業のお知らせ",
+        published: true,
+    },
+    {
+        id: 2,
+        title: "システムメンテナンス",
+        published: false,
+    },
+    {
+        id: 3,
+        title: "新サービス開始",
+        published: true,
+    },
+];
+
+function getPublishedTitles(noticeList) {
+    return noticeList
+        .filter((notice) => notice.published === true)
+        .map((notice) => notice.title);
+}
+
+console.log(getPublishedTitles(notices));
+console.log("test");
